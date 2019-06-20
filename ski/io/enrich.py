adde64a84f772fc9152da1c5119ae5d7b7a6c150
Steve Roberts <steve.roberts@essarsoftware.co.uk>
2019-06-20 15:20:47 +0100

    Refactored enrich module

--- ski/io/enrich.py
+++ ski/io/enrich.py
@@ -17,120 +17,62 @@ class PointWindow:
     MIDPOINT = 2
     BACKWARD = 3
 
-    def __init__(self, points, window_type, size, position=0):
+    def __init__(self, window_type, size):
 
         # Do not allow a midpoint to be created of even length
         if window_type == PointWindow.MIDPOINT and size % 2 == 0:
             raise ValueError('Midpoint window must be of odd length')
 
-        self.points = points
         self.window_type = window_type
         self.size = size
-        self.position = position
-        self.window_full = False
 
 
-    def window(self):
+    def for_points(self, points, position=0):
         if self.window_type == PointWindow.FORWARD:
             # Forward load from the position
-            window_start = max(0, self.position)
-            window_end = min(len(self.points), (self.position + self.size))
-            log.debug('Forward window from %d: %d->%d', self.position, window_start, window_end)
+            window_start = max(0, position)
+            window_end = min(len(points), (position + self.size))
 
         elif self.window_type == PointWindow.MIDPOINT:
             # Load equally either side of position
             mp = self.size / 2
 
-            window_start = max(0, self.position - (min(len(self.points), (self.position + ceil(mp))) - self.position) + 1, self.position - floor(mp))
-            window_end = min(len(self.points), self.position + (self.position - max(0, self.position - floor(mp))) + 1, (self.position + ceil(mp)))
-            log.debug('Midpoint window from %d: %d->%d', self.position, window_start, window_end)
+            window_start = max(0, position - (min(len(points), (position + ceil(mp))) - position) + 1, position - floor(mp))
+            window_end = min(len(points), position + (position - max(0, position - floor(mp))) + 1, (position + ceil(mp)))
 
         elif self.window_type == PointWindow.BACKWARD:
             # Backward load from the position
-            window_start = max(0, self.position - self.size + 1)
-            window_end = min(len(self.points), self.position + 1)
-            log.debug('Backward window from %d: %d->%d', self.position, window_start, window_end)
+            window_start = max(0, position - self.size + 1)
+            window_end = min(len(points), position + 1)
 
-        window = self.points[window_start:window_end]
-        self.window_full = (len(window) == self.size)
-        log.debug('Window is %s', 'full' if self.window_full else 'not full')
+        window = points[window_start:window_end]
+        window_full = (len(window) == self.size)
+        log.debug('Window is %s', 'full' if window_full else 'not full')
         return window
 
 
-    def alt_delta(self):
-        return self.window()[-1].alt - self.window()[0].alt
 
-
-    def alt_cuml_gain(self):
-        return sum([p.alt_d for p in self.window() if p.alt_d > 0])
-
-
-    def alt_cuml_loss(self):
-        return sum([p.alt_d for p in self.window() if p.alt_d < 0])
-
-
-    def alt_max(self):
-        return max([p.alt for p in self.window()])
-
-
-    def alt_min(self):
-        return min([p.alt for p in self.window()])
-
-
-    def distance(self):
-        return sum([p.dst for p in self.window()])
-
-
-    def speed_ave(self):
-        return avg([p.spd for p in self.window()])
-
-
-    def speed_delta(self):
-        return self.window()[-1].spd - self.window()[0].spd
-
-
-    def speed_max(self):
-        return max([p.spd for p in self.window()])
-
-
-    def speed_min(self):
-        return min([p.spd for p in self.window()])
-
-
-
-def avg(values):
+def __avg(values):
     return float(sum(values)) / max(1, len(values))
 
 
-
-
-def __enriched_alt_vals(window):
+def __enriched_alt_vals(window, points, position):
     return {
-        'alt_delta' : window.alt_delta(),
-        'alt_gain'  : window.alt_cuml_gain(),
-        'alt_loss'  : window.alt_cuml_loss(),
-        'alt_max'   : window.alt_max(),
-        'alt_min'   : window.alt_min()
+        'alt_delta' : window_alt_delta(window, points, position),
+        'alt_gain'  : window_alt_cuml_gain(window, points, position),
+        'alt_loss'  : window_alt_cuml_loss(window, points, position),
+        'alt_max'   : window_alt_max(window, points, position),
+        'alt_min'   : window_alt_min(window, points, position)
     }
 
 
-def __enriched_speed_vals(window):
+def __enriched_speed_vals(window, points, position):
     return {
-        'speed_min'   : window.speed_min(),
-        'speed_max'   : window.speed_max(),
-        'speed_ave'   : window.speed_ave(),
-        'speed_delta' : window.speed_delta()
+        'speed_min'   : window_speed_min(window, points, position),
+        'speed_max'   : window_speed_max(window, points, position),
+        'speed_ave'   : window_speed_ave(window, points, position),
+        'speed_delta' : window_speed_delta(window, points, position)
     }
-
-
-def get_enriched_data(window):
-    # Build a dict of enriched values
-    data = {
-        'distance' : window.distance()
-    }
-    data.update(__enriched_alt_vals(window))
-    data.update(__enriched_speed_vals(window))
-    return data
 
 
 def enrich_points(points, windows, head=[], min_tail=1, overflow=[]):
@@ -144,16 +86,13 @@ def enrich_points(points, windows, head=[], min_tail=1, overflow=[]):
         for k in windows:
             # Get window based on key and set to current position
             w = windows[k]
-            w.position = position
-
-            if w.points[w.position] != p:
-                log.warning('Window point is not equal to the main point')
 
             # Build a dict of enriched values
-            vals = get_enriched_data(w)
+            vals = get_enriched_data(w, points, position)
 
             # Add the enrichment data to the point, using the input dict key
             p.windows[k] = EnrichedWindow(**vals)
+            log.debug('Enriched data: %s=%s', k, vals)
 
         log.info('Added windows: %s', list(p.windows.keys()))
 
@@ -162,4 +101,64 @@ def enrich_points(points, windows, head=[], min_tail=1, overflow=[]):
         
     # Return any unprocessed elements
     overflow.extend(points[position:])
+
+
+def get_enriched_data(window, points, position=0):
+    # Build a dict of enriched values
+    data = {
+        'distance' : window_distance(window, points, position)
+    }
+    data.update(__enriched_alt_vals(window, points, position))
+    data.update(__enriched_speed_vals(window, points, position))
+    return data
+
+
+def window_alt_delta(window, points, position):
+    w = window.for_points(points, position)
+    return w[-1].alt - w[0].alt
+
+
+def window_alt_cuml_gain(window, points, position):
+    w = window.for_points(points, position)
+    return sum([p.alt_d for p in w if p.alt_d > 0])
+
+
+def window_alt_cuml_loss(window, points, position):
+    w = window.for_points(points, position)
+    return sum([p.alt_d for p in w if p.alt_d < 0])
+
+
+def window_alt_max(window, points, position):
+    w = window.for_points(points, position)
+    return max([p.alt for p in w])
+
+
+def window_alt_min(window, points, position):
+    w = window.for_points(points, position)
+    return min([p.alt for p in w])
+
+
+def window_distance(window, points, position):
+    w = window.for_points(points, position)
+    return sum([p.dst for p in w])
+
+
+def window_speed_ave(window, points, position):
+    w = window.for_points(points, position)
+    return __avg([p.spd for p in w])
+
+
+def window_speed_delta(window, points, position):
+    w = window.for_points(points, position)
+    return w[-1].spd - w[0].spd
+
+
+def window_speed_max(window, points, position):
+    w = window.for_points(points, position)
+    return max([p.spd for p in w])
+
+
+def window_speed_min(window, points, position):
+    w = window.for_points(points, position)
+    return min([p.spd for p in w])
 
