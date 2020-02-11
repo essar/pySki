a5c5a07634a028d4035bed07e90db994643cd122
Steve Roberts <steve.roberts@essarsoftware.co.uk>
2020-02-11 17:31:35 +0000

    Test script cleanup

--- tests/scripts/gpx-file-to-directory.py
+++ tests/scripts/gpx-file-to-directory.py
@@ -8,7 +8,7 @@ import time
 from datetime import time as datetime
 from math import floor
 from ski.logging import calc_stats
-from ski.io.gpx import GPXSource, stats as loader_stats
+from ski.io.gpx import GPXSource, stats as reader_stats
 from ski.io.track import TrackFileLoader
 from ski.loader.cleanup import stats as cleanup_stats
 from ski.loader.dataloader import file_to_directory, direct_write_stats as write_stats
@@ -33,14 +33,14 @@ end = time.time()
 
 print('Execution completed in {:.2f} seconds'.format(end - start))
 
-total_time = sum([x['process_time'] for x in [loader_stats, cleanup_stats, write_stats]])
+total_time = sum([x['process_time'] for x in [reader_stats, cleanup_stats, write_stats]])
 duration = datetime(0, floor(total_time / 60), floor(total_time % 60), floor(total_time % 1))
 duration_str = duration.strftime('%Mm%Ss')
 
 print('                    process time ')
 print(' phase     points     (s)    (%) ')
 print('---------------------------------')
-print(' {:8s}  {:>6d}  {:>6.3f}  {:>4.1f}%'.format('loader', *calc_stats(loader_stats, total_time)))
+print(' {:8s}  {:>6d}  {:>6.3f}  {:>4.1f}%'.format('loader', *calc_stats(reader_stats, total_time)))
 print(' {:8s}  {:>6d}  {:>6.3f}  {:>4.1f}%'.format('cleanup', *calc_stats(cleanup_stats, total_time)))
 print(' {:8s}  {:>6d}  {:>6.3f}  {:>4.1f}%'.format('write', *calc_stats(write_stats, total_time)))
 print('---------------------------------')
